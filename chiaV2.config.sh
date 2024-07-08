2b34367a5829285733a3dc352db7b3e64d2c32e3
fengchuiguo1994 <huang182@live.cn>
2024-07-08 19:44:03 +0800

    Update chiaV2.config.sh

--- chiaV2.config.sh
+++ chiaV2.config.sh
@@ -1,31 +1,58 @@
 #!/bin/bash
 
-NTHREAD=10 #cpu usage per job
-ChIApipeV2="/data/home/ruanlab/huangxingyu/20230912/testtmp/chiaV2.sif"
-# R1suf="_R1.fastq.gz" # suffix of the fastq files, modify if different
-# R2suf="_R2.fastq.gz" # read2 suffix
-R1suf="R1_001.fastq.gz"
-R2suf="R3_001.fastq.gz"
+## Config file for CPU pipeline
 
-### cpu parameters
-genome="/data/home/ruanlab/huangxingyu/Haoxi20230315/20230205_pipeline/refs/mm10/bwa/mm10.fa"
-linker=ACGTGATATTTCACGACTCT ## linker 3
-minMapScore=30 #this is the default CPU memaln set
-blacklist="null" # default: null
-
-#cluster parameters:
-addNone="T" # "T": PET from reads with linker and not. "F": PET from reads with linker.
+#Define directories & filenames
+R1suf="_R1.fastq.gz" # suffix of the fastq files, modify if different
+R2suf="_R2.fastq.gz" # read2 suffix
 
 #Chiapet params
 extbp=500
 selfbp=8000
+minMapScore=30 #this is the default CPU memaln set
 
+#genome='mm10' #genome version
+
+genome="/data/home/ruanlab/huangjiaxiang/pipeline/shell_version/mm10.fa"
+blackbed="null"
+ChIApipeV2="/data/home/ruanlab/huangjiaxiang/software/chiav2.sif"
 #Peak calling params
-genomelen='hs' #used in macs: human='hs' mouse='mm' fly='dm' or 2.7e9
-macsq=0.001
+organism='mm' #used in macs: human='hs'  mouse='mm'  fly='dm'
+macsq=0.000001
+#macsq=0.01
 peakext=150
 shiftsize=$(( -$peakext/2 ))
-allreads="F" # "T": will use all reads, "F": just use self lagation reads
 
 #Chiasig params
 PET=3
+
+### linker seq
+linker=ACGTGATATTTCACGACTCT ## linker 3
+
+#cluster job setting parameters:
+NTHREAD=10 #cpu usage per job
+hrs=72 #number of hours allocation
+GB=72 #memory in GB
+seed=12639
+
+#matrix params
+binsize=100000 # matrix resolution in bp
+
+
+## Not important for running, metadata only
+
+# The type of sequencing run:
+#    "miseq" - around 30 million reads
+#    "nextseq" - around 300 million reads
+#    "hiseq" - around 300 million reads
+#    "pooled" - around 1 billion reads
+run_type="HiMi"
+
+# The factor for which the IP was performed
+ip_factor="ATAC"
+
+# Cell type
+cell_type="NA"
+
+
+
